6706420e6150de036cee17214ea87e6fcb2f1e91
ArthurBonsu <peeman34@gmail.com>
2021-04-30 01:50:13 +0800

    UPDATED

--- src/contracts/Uniswap.sol
+++ src/contracts/Uniswap.sol
@@ -15,7 +15,26 @@ interface IUniswap{
 
     }
   
-  contract MyDefiProject
+  contract MyDefiProject{
+     IUniswap uniswap;
+
+     constructor (address _uniswap){
+       uniswap = IUniswap(_uniswap);
+     }
+
+  function swapTokensForEth( 
+    address token, uint amountIn, uint amountOutMin,
+    uint deadline) external {
+  
+  IERC20(token).transferFrom(msg.sender, address(this), amountIn)
+ 
+ // address of the two tokens, wonder why they call it path
+  address[] memory path = new address[](2);
+  path[0] = token;
+  path[1] = uniswap.WETH();
+  IERC20(token).approve(address(uniswap), amountIn);
+  uniswap.swapExactTokensForEth(amountIn, amountOutMin, path, msg.sender, deadline);
+  }
 
     
     }
